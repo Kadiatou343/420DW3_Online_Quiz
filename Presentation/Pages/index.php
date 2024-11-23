fc243159f7f08ba9c188769f004b4d07928cd962
Kadiatou <kimkd08@gmail.com>
2024-11-22 21:25:54 -0500

    index, presentation, start homegamer

---
+++ Presentation/Pages/index.php
@@ -0,0 +1,52 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Parkinsans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../StyleSheets/indexStyle.css">
+    <title>Presentation</title>
+</head>
+<body>
+        <div class="header">
+            <img src="../images/quizzy_dev_logo.png" alt="logo" width="70" height="60">
+        </div>
+        <div class="content">
+            <div class="section">
+                <p>Etes-vous developpeur junior🧑🏼‍💻? Ou bien étudiant en programmation informatique?</p>
+                <p>Ou tout simplement fan de ce domaine?</p>
+                <p>Si oui, restez🙃&nbsp;! <strong>Quizzy&nbsp;Dev</strong> est fait pour vous&nbsp;! Vous ne serez pas deçu😊&nbsp;!</p>
+                <p>
+                    Et bien, pour la petite histoire, <strong>Quizzy&nbsp;Dev</strong> est partie d'un projet academique et 
+                    <br> ça a finit par une histoire d'amour avec sa créatrice❤️
+                </p>
+                <p>
+                    <strong>Quizzy&nbsp;Dev</strong> est un site web qui vous permet de participer à des quiz
+                    qui ont tous <br> un rapport avec la programmation informatique, principalement les celèbres <br> langages de programmation.
+                </p>
+                <p>
+                    Croyez-moi quand je vous dis que vous allez vous amusez😁
+                </p>
+            </div>
+            <div class="section">
+                <p>Pour s'incrire et commencer à avoir du fun,&nbsp;<a href="./register.php">c'est&nbsp;par&nbsp;ici</a></p>
+                <p>Vous avez déjà un compte?&nbsp;<a href="./login.php">Continuez&nbsp;ici</a></p>
+                <p>Voulez-vous devenir administrateur&nbsp;? Veuillez contactez le support!&nbsp; <a href="#">Plus&nbsp;d'infos</a></p>
+            </div>
+        </div>
+        <div class="footer">
+            <p>Made by Kadiatou Doumbia</p>
+            <p><i class="bi bi-c-circle"></i>&nbsp;Tous droits réservés</p>
+        </div>
+</body>
+</html>
